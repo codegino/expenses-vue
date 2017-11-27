597aa244bcb149eeb3b38d15bb78b9b87d8afdbe
gihooh <carloginocatapang@gmail.com>
2017-11-27 23:10:53 +0800

    Added support for adding with predefined dates.

--- src/components/pages/DailyExpenses.vue
+++ src/components/pages/DailyExpenses.vue
@@ -2,14 +2,16 @@
   <div class="container">
     <h1>Expenses</h1>
     <template v-for="(dailyExpenses, index) in monthlyExpenses">
-      <h5>{{index}}</h5>
+      <h5 class="glyphicon-plus-sign">{{index}}
+        <a href="#"><span @click="showModal(new Date(index))">+</span></a>
+      </h5>
       <p v-for="expenses in dailyExpenses">{{expenses.name}}: P{{expenses.price}}</p>
       <h4>Total: {{computeTotalForEachDay(dailyExpenses)}}</h4>
       <hr>
     </template>
     <h3>Grand Total: {{grandTotal}}</h3>
-    <b-btn v-b-modal.addDataModal>Add</b-btn>
-    <b-modal id="addDataModal" title="Enter something" @ok="addEntry" ok-only>
+    <b-btn @click="showModal(new Date())">Add</b-btn>
+    <b-modal ref="myModalRef" id="addDataModal" title="Enter something" @ok="addEntry" ok-only>
       <add-new-item></add-new-item>
     </b-modal>
   </div>
@@ -38,6 +40,11 @@
       AddNewItem
     },
     methods: {
+      showModal (date) {
+        console.log(date)
+        this.$store.commit('newItem/date', date)
+        this.$refs.myModalRef.show()
+      },
       computeTotalForEachDay (expenses) {
         let total = 0
         for (let i = 0; i < expenses.length; i++) {
